8218e839787914b779551512c5c160360cfe145b
aurawolff <deepaman0556@gmail.com>
2025-11-20 22:04:23 +0530

    feat(backend):add express-rate-limit to limit requests in given period of time

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import { protect } from "./middlewares/auth.middleware.js";
 import projectRouter from "./routes/project.route.js";
 import taskRouter from "./routes/task.route.js";
 import commentRouter from "./routes/comment.route.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,9 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(clerkMiddleware());
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+});
+
 // todo create an healthcheck route for checking server is live
 app.get("/", (req, res) => res.send("server is live"));
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use(limiter);
 
 // Routes
 app.use("/api/workspaces", workspaceRouter);
